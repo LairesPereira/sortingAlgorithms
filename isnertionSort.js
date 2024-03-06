3ae71654ab67b707d17ce47086b35c6cbce2eea0
Laires <lairespsoares@gmail.com>
2024-03-05 21:16:17 -0300

    nova funcao

--- isnertionSort.js
+++ isnertionSort.js
@@ -1,18 +1,27 @@
-let randomInput = [ 8, 3, 5, 1, 4, 2, 9]
+let randomInput = Array.from({ length: 200 }, () => Math.floor(Math.random() * 200)) 
 let position = 0;
 let key = randomInput[0]
 
-let aux 
-for (let i = 0; i < randomInput.length; i++) {
-    let element = i
-    while(element > 0 && randomInput[element - 1] > randomInput[element]){
-        aux = randomInput[element - 1]
-        randomInput[element - 1] = randomInput[element]
-        randomInput[element] = aux
-        element = element - 1
+function insertionSort(randomArray) {
+    console.log('Random:', randomArray)
+    console.time('Execution Time')
+    
+    let aux 
+    for (let i = 0; i < randomInput.length; i++) {
+        let element = i
+        while(element > 0 && randomInput[element - 1] > randomInput[element]){
+            aux = randomInput[element - 1]
+            randomInput[element - 1] = randomInput[element]
+            randomInput[element] = aux
+            element = element - 1
+        }
     }
+
+    console.timeEnd('Execution Time');
+    console.log('Sorted: ', randomArray)
 }
 
-console.log(randomInput)
+
+insertionSort(randomInput)
 
 // [ 3 5 8 1 4 2 ]
